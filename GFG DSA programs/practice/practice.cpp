9ece564c13ed76e7218d1b4bc0fb5f5fd6e74e34
Rishi Rusia <Rishirusia1@gmail.com>
2022-10-12 17:54:26 +0530

    yay

---
+++ GFG DSA programs/practice/practice.cpp
@@ -0,0 +1,32 @@
+// create table stdmarks(stdid number primary key, stdname varchar(20), marks number, stdclass number, stdsection varchar(1));
+// insert into stdmarks(stdid, stdname, marks, stdclass, stdsection) values(1, 'Rishi', 66, 10, 'a');
+// insert into stdmarks(stdid, stdname, marks, stdclass, stdsection) values(2, 'Vishy', 66, 10, 'a');
+// insert into stdmarks(stdid, stdname, marks, stdclass, stdsection) values(3, 'Ram', 66, 10, 'a');
+// insert into stdmarks(stdid, stdname, marks, stdclass, stdsection) values(4, 'Anjan', 66, 10, 'a');
+// insert into stdmarks(stdid, stdname, marks, stdclass, stdsection) values(5, 'Gaurav', 66, 10, 'a');
+
+// select *from stdmarks;
+
+// delete from stdmarks where stdname = 'Ram';
+
+// update stdmarks set marks = 92;
+
+// select *from stdmarks;
+
+// //question 2
+
+// declare
+
+//     a1 number;
+// b1 number;
+// c1 number;
+
+// begin
+
+//     a1 = 7;
+// b1 = 7;
+// c1 = a1 + b1;
+
+// dbms_output.put_line(c1);
+
+// end;
